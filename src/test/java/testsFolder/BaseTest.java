03f05d6102866b0001781cc42d7aa63b58ae6373
GostryMaxim <rotoff7@mail.ru>
2023-08-23 23:17:02 +0300

    first case

--- src/test/java/testsFolder/BaseTest.java
+++ src/test/java/testsFolder/BaseTest.java
@@ -1,6 +1,12 @@
 package testsFolder;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
+    @BeforeAll
+     static void beforeTests(){
+    }
 }
--- src/test/java/testsFolder/TestPractice.java
+++ src/test/java/testsFolder/TestPractice.java
@@ -1,4 +1,74 @@
 package testsFolder;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 public class TestPractice {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void beforeTests() {
+
+        ChromeOptions co = new ChromeOptions();
+        co.setBinary("E:\\TestChrome\\chrome-win64\\chrome.exe");
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver(co);
+
+    }
+
+    @Test
+    void firstTest() {
+
+        driver.get("http://localhost:8080/food");
+        driver.manage().window().maximize();
+
+//        Попробовал отловить ошибку (только для самой таблицы).
+        try {
+            WebElement productTable = driver.findElement(By.xpath("//h5[text()='Список товаров']"));
+        } catch (NoSuchElementException e) {
+            System.out.println("Элемент не найден");
+            fail("Table not found");
+        }
+        WebElement idColumn = driver.findElement(By.xpath("//tr/th[text()='#']"));
+        WebElement nameColumn = driver.findElement(By.xpath("//tr/th[text()='Наименование']"));
+        WebElement typeColumn = driver.findElement(By.xpath("//tr/th[text()='Тип']"));
+        WebElement exoticColumn = driver.findElement(By.xpath("//tr/th[text()='Экзотический']"));
+        WebElement addGoodsButton = driver.findElement(By.xpath(
+                "//button[@class='btn btn-primary' and text()='Добавить']"));
+
+//        Получаем информацию о цвете кнопки
+        String backgroundColor = addGoodsButton.getCssValue("background-color");
+        String borderColor = addGoodsButton.getCssValue("border-color");
+
+//        Проверяем соответствие цвета
+        if (backgroundColor.equals("rgba(0, 123, 255, 1)") && borderColor.equals("rgb(0, 123, 255)")){
+            System.out.println("Button color is correct");
+        } else {
+            fail("Wrong button color");
+        }
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @AfterAll
+    static void afterTests() {
+//        driver.quit();
+    }
 }
